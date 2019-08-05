6a6a1533ccef3d16a57450c7264937d53323720b
MarkNahabedian <naha@mit.edu>
2019-08-05 13:19:36 -0400

    Allow for unknown/defauklt package name.

--- util/implname.go
+++ util/implname.go
@@ -5,6 +5,9 @@ import "fmt"
 // ImplName returns the name of the struct that defimpl will define
 // for an interface of the specified name.
 func ImplName(package_name string, interface_name string) string {
+	if package_name == "" {
+		return interface_name
+	}
 	return fmt.Sprintf("%s.%s", package_name, interface_name)
 }
 
