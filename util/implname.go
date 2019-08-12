3480ce3435981d12f8e81f20696f70b87bd023ec
MarkNahabedian <naha@mit.edu>
2019-08-11 21:04:25 -0400

    Must get the right name both places.

--- util/implname.go
+++ util/implname.go
@@ -6,7 +6,7 @@ import "fmt"
 // for an interface of the specified name.
 func ImplName(package_name string, interface_name string) string {
 	if package_name == "" {
-		return interface_name
+		return fmt.Sprintf("%sImpl", interface_name)
 	}
 	return fmt.Sprintf("%s.%sImpl", package_name, interface_name)
 }
