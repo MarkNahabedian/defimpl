59a82a8e421390e37fe38f2506597786579f53bb
MarkNahabedian <naha@mit.edu>
2020-11-29 20:40:39 -0500

    AstMatch

---
+++ util/ast_match.go
@@ -0,0 +1,81 @@
+package util
+
+import "fmt"
+import "reflect"
+import "strings"
+import "go/ast"
+import "go/token"
+
+
+// AstMatch matches a candidate ast.Node against a pattern ast.Node.
+// If the name of an ast.Ident in pattern starts with '_', then that
+// name and the corresponding value from candidate are added to
+// scratchpad.
+func AstMatch(pattern interface{}, candidate interface{}, scratchpad map[string]interface{}) (bool, error) {
+	if reflect.TypeOf(pattern) != reflect.TypeOf(candidate) {
+		return false, fmt.Errorf("type mismatch: %T, %T", pattern, candidate)
+	}
+	switch p := pattern.(type) {
+	case token.Pos:
+		// We don't care whether positions match.
+		return true, nil
+	case *ast.Ident:
+		c := candidate.(*ast.Ident)
+		if strings.HasPrefix(p.Name, "_") {
+			val, ok := scratchpad[p.Name]
+			if ok {
+				if name, ok := val.(string); ok {
+					if name != c.Name {
+						return false, fmt.Errorf("identifier mismatch: %s, %s", name, c.Name)
+					}
+				} else {
+					return false, fmt.Errorf("scratchpad[%s] is %T, not string", p.Name, val)
+				}
+			} else {
+				scratchpad[p.Name] = c.Name
+			}
+		}
+	default:
+		pv := reflect.ValueOf(pattern)
+		cv := reflect.ValueOf(candidate)
+		if pv.Kind() == reflect.Array || pv.Kind() == reflect.Slice {
+			if pvl, cvl :=  pv.Len(), cv.Len(); pvl != cvl {
+				return false, fmt.Errorf("Lengths don't match: %d, %d", pvl, cvl)
+			} else {
+				for i := 0; i < pvl; i++ {
+					match, err := AstMatch(
+						pv.Index(i).Interface(),
+						cv.Index(i).Interface(),
+						scratchpad)
+					if !match {
+						return false, err
+					}
+				}
+			}
+			return true, nil
+		}
+		
+		if pv.Kind() != reflect.Ptr {
+			panic(fmt.Errorf("%T %T not a pointer", pattern, candidate))
+		}
+		pvi := reflect.Indirect(pv)
+		cvi := reflect.Indirect(cv)
+		if pvi.Kind() == reflect.Invalid && cvi.Kind() == reflect.Invalid {
+			// Null pointers match.
+			return true, nil
+		}
+		if pvi.Kind() != reflect.Struct || cvi.Kind() != reflect.Struct {
+			panic(fmt.Errorf("%T, %T not struct pointer", pattern, candidate))
+		}
+		for i := 0; i < reflect.Indirect(pv).NumField(); i++ {
+			pf := pvi.Field(i).Interface()
+			cf := cvi.Field(i).Interface()
+			// fmt.Printf("Considering %v %v\n", pf, cf)
+			if match, err := AstMatch(pf, cf, scratchpad); !match {
+				return false, err
+			}
+		}
+	}
+	return true, nil
+}
+
---
+++ util/ast_match_test.go
@@ -0,0 +1,45 @@
+package util
+
+import "testing"
+import "go/ast"
+import "go/parser"
+import "go/token"
+
+
+const pattern = `
+package foo
+
+type myInterface interface {
+	Read() string
+}
+
+// _METHOD_NAME is part of the _INTERFACE_NAME interface.
+func (x *_STRUCT_NAME) _METHOD_NAME() _SLOT_TYPE {
+	return x._SLOT_NAME
+}
+`  // End
+
+func TestASTMatch(t *testing.T) {
+	fset := token.NewFileSet()
+	parsed, err := parser.ParseFile(fset, "pattern", pattern, parser.ParseComments)
+	if err != nil {
+		t.Fatalf("Error while parsing pattern: %s", err)
+		return
+	}
+	show := func(what string, x interface{}) {
+		t.Logf("%s: %T %v", what, x, x)
+	}
+	my_interface := parsed.Decls[0].(*ast.GenDecl).Specs[0].(*ast.TypeSpec)
+	method := parsed.Decls[1].(*ast.FuncDecl)
+	show("my_interface", my_interface)
+	show("method", method)
+	expectType := method.Type
+	gotType := my_interface.Type.(*ast.InterfaceType).Methods.List[0].Type
+	show("method FuncType", expectType)
+ 	show("my_interface 1st method Type", gotType)
+	scratchpad :=  map[string]interface{}{}
+	matched, err := AstMatch(expectType, gotType, scratchpad)
+	t.Logf("AstMatch result %v %v %#v", matched, err, scratchpad)
+	t.Fail()
+}
+
